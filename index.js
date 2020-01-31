2b2d439fcc66dc4f6619d0072a22f438d1db37a2
Emery Tate <tateejt@gmail.com>
2020-01-31 04:41:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ function walkToPark(dogName, dogBreed) {
   console.log(`Walk to the park with ${dogName} the  ${dogBreed}`);
 }
 function throwFrisbee(dogName, dogBreed) {
- console.log(`Throw the frisbee for ${dogName} the poodle`);
+ console.log(`Throw the frisbee for ${dogName} the  ${dogBreed}`);
 }
 function walkHome(dogName, dogBreed) {
-  console.log(`Walk home with ${dogName} the poodle`);
+  console.log(`Walk home with ${dogName} the  ${dogBreed}`);
 }
 function unleashDog(dogName, dogBreed) {
   console.log(`Unleash ${dogName} the poodle`);
