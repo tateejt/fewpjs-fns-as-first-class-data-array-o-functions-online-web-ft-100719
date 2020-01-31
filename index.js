83da4e8d17e158a6aafa8f89d0f86d102f074f0e
Emery Tate <tateejt@gmail.com>
2020-01-31 04:36:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 function wakeDog(dogName, dogBreed) {
   console.log(`Wake ${dogName} the ${dogBreed}`);
-  console.log(`Leash ${dogName} the ${dogBreed}`);
-  console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
-  console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
-  console.log(`Walk home with ${dogName} the ${dogBreed}`);
-  console.log(`Unleash ${dogName} the ${dogBreed}`);
+
 }
